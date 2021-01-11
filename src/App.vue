a3dac79220b5a907c953a31e335ed799447640da
Dutchbet <wouterholtrop@hotmail.nl>
2021-01-11 15:46:42 +0100

    Old

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app id="nlr-webapp">
+    <v-navigation-drawer app
+    permanent
+    left
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Dashboard
+          </v-list-item-title>
+            <!-- Subtitel voor de navigation drawer
+            <v-list-item-subtitle>
+            </v-list-item-subtitle>
+            -->
+        </v-list-item-content>
+      </v-list-item>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+     <v-container class="my-12">
+    <v-main>
+      <router-view>
+      </router-view>
+    </v-main>
+    </v-container> 
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+        items: [
+          { title: 'Database', icon: 'mdi-poll', to: '/' },
+          { title: 'Recent Activities', icon: 'mdi-email-open', to: '/activities' },
+        ],
+          
+    //
+  }),
+};
+</script>
---
+++ src/views/Activities.vue
@@ -0,0 +1,30 @@
+<template>
+<v-container class="my-12">
+  <div class="Activities">
+    <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+  </div>
+</v-container>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+import { Plotly } from 'vue-plotly'
+
+export default {
+  name: 'Activities',
+  components: {
+    Plotly
+  },
+  data:{
+    data:[{
+      x: [1,2,3,4],
+      y: [10,15,13,17],
+      type:"scatter"
+    }],
+    layout:{
+      title: "My graph"
+    }
+  }
+}
